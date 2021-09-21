6b9b2ee57b37975e7532863c9fde793be147c67b
rani1029 <ranig1029@gmail.com>
2021-09-21 12:04:11 +0530

    permutation problem done

---
+++ AlgorithmsProblem/Permutation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Permutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
